374b4560891005ad5468fe390976b4a6778513bb
Acassio Fernando <acassio.fernando@hotmail.com>
2024-09-19 15:08:16 -0300

    :test_tube: test: Criando teste de validação de data de transferência retroativa

--- domain/src/test/java/com/tokio/transfer/scheduler/domain/transference/TransferenceTest.java
+++ domain/src/test/java/com/tokio/transfer/scheduler/domain/transference/TransferenceTest.java
@@ -11,8 +11,8 @@ public class TransferenceTest {
 
     @Test
     public void givenAValidParams_whenCallNewTransference_thenInstantiateATransference() {
-        final var expectedSourceAccount = "XXXXXXXXXX";
-        final var expectedDestinationAccount = "XXXXXXXXXX";
+        final var expectedSourceAccount = "0123456789";
+        final var expectedDestinationAccount = "9876543210";
         final var expectedAmount = 99.99;
         final var expectedTax = 2.5;
         final var expectedTransferDate = "25/09/2024";
@@ -38,8 +38,29 @@ public class TransferenceTest {
         final String expectedTransferDate = null;
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = "'transferDate' should not be null";
-        final var expectedSourceAccount = "XXXXXXXXXX";
-        final var expectedDestinationAccount = "XXXXXXXXXX";
+        final var expectedSourceAccount = "0123456789";
+        final var expectedDestinationAccount = "9876543210";
+        final var expectedAmount = 99.99;
+        final var expectedTax = 2.5;
+        final var expectedIsActive = true;
+
+        final var actualTransference =
+                Transference.newTransference(expectedSourceAccount, expectedDestinationAccount, expectedAmount, expectedTax, expectedTransferDate, expectedIsActive);
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> actualTransference.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).getMessage());
+    }
+
+    @Test
+    public void givenAnInvalidRetroactiveTransferDate_whenCallNewTransferenceAndValidate_thenShouldReceiveError() {
+        final String expectedTransferDate = "01/01/2024";
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'transferDate' should not be earlier than the current date";
+        final var expectedSourceAccount = "0123456789";
+        final var expectedDestinationAccount = "9876543210";
         final var expectedAmount = 99.99;
         final var expectedTax = 2.5;
         final var expectedIsActive = true;
