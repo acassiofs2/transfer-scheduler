64034b8dca30c37ac9b8e0e7aa4beb565714f6f0
Acassio Fernando <acassio.fernando@hotmail.com>
2024-09-20 17:24:05 -0300

    :sparkles: feat: implementando classe CreateTransferenceUseCase e importando biblioteca vavr

--- application/src/main/java/com/tokio/transfer/scheduler/application/transference/create/CreateTransferenceUseCase.java
+++ application/src/main/java/com/tokio/transfer/scheduler/application/transference/create/CreateTransferenceUseCase.java
@@ -1,8 +1,9 @@
 package com.tokio.transfer.scheduler.application.transference.create;
 
 import com.tokio.transfer.scheduler.application.UseCase;
+import com.tokio.transfer.scheduler.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
-/*public abstract class CreateTransferenceUseCase
-        extends UseCase<CreateTransferenceCommand> {
+public abstract class CreateTransferenceUseCase
+        extends UseCase<CreateTransferenceCommand, Either<Notification, CreateTransferenceOutput>> {
 }
-*/
