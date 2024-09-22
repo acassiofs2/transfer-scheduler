0fabdc123fa8ce18a1602e948cab41851d07c84c
Acassio Fernando <acassio.fernando@hotmail.com>
2024-09-22 14:13:25 -0300

    :test_tube: test: Criando teste de listagem de transferencias

--- infrastructure/src/test/java/com/tokio/transfer/scheduler/infrastructure/transference/TransferenceMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/tokio/transfer/scheduler/infrastructure/transference/TransferenceMySQLGatewayTest.java
@@ -1,13 +1,17 @@
 package com.tokio.transfer.scheduler.infrastructure.transference;
 
+import com.tokio.transfer.scheduler.domain.pagination.SearchQuery;
 import com.tokio.transfer.scheduler.domain.transference.Transference;
 import com.tokio.transfer.scheduler.domain.user.UserID;
 import com.tokio.transfer.scheduler.domain.utils.DateUtils;
 import com.tokio.transfer.scheduler.infrastructure.MySQLGatewayTest;
+import com.tokio.transfer.scheduler.infrastructure.transference.persistence.TransferenceJpaEntity;
 import com.tokio.transfer.scheduler.infrastructure.transference.persistence.TransferenceRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @MySQLGatewayTest
 public class TransferenceMySQLGatewayTest {
@@ -47,15 +51,33 @@ public class TransferenceMySQLGatewayTest {
 
     @Test
     public void givenPrePersistedTransferences_whenCallsFindAll_shouldReturnPaginated() {
+        final var expectedUserId = UserID.unique();
+
         final var expectedPage = 0;
         final var expectedPerPage = 1;
         final var expectedTotal = 2;
 
         final var transference1 = Transference.newTransference
-                ("0123456789", "9876543210", 99.99, DateUtils.now().plusDays(11), true);
+                (expectedUserId,"0123456789", "9876543210", 99.99, DateUtils.now().plusDays(11), true);
         final var transference2 = Transference.newTransference
-                ("1111111111", "2222222222", 79.99, DateUtils.now().plusDays(21), true);
+                (expectedUserId,"1111111111", "2222222222", 79.99, DateUtils.now().plusDays(21), true);
 
+        Assertions.assertEquals(0, transferenceRepository.count());
 
+        transferenceRepository.saveAll(List.of(
+                TransferenceJpaEntity.from(transference1),
+                TransferenceJpaEntity.from(transference2)
+        ));
+
+        Assertions.assertEquals(2, transferenceRepository.count());
+
+        final var query = new SearchQuery(0, 1, "", "id", "asc");
+        final var actualResult = transferenceGateway.findAll(query);
+
+        Assertions.assertEquals(expectedPage, actualResult.getCurrentPage());
+        Assertions.assertEquals(expectedPerPage, actualResult.getPerPage());
+        Assertions.assertEquals(expectedTotal, actualResult.getTotal());
+        Assertions.assertEquals(expectedPerPage, actualResult.getItems().size());
+        Assertions.assertEquals(transference1.getId(), actualResult.getItems().get(0).getId());
     }
 }
